0e6d55273cecdde6737329497c5c17ddf846a261
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-01-03 17:10:06 -0500

    changing config

--- shortcut.sh
+++ shortcut.sh
@@ -2,14 +2,18 @@
 cd /Users/yiwenchen/Desktop/ResearchProjects/scripts
 source venv/bin/activate
 
+cd /projects/vig/yiwenc/ResearchProjects/lightingDiffusion/3dgs/LVSMExp
+conda activate /projects/vig/yiwenc/all_env/rayzer
 
+# data preprocessing
+# re10k
 python process_data.py --base_path re10k --output_dir re10k_processed --mode test
 
+# objaverse
 python preprocess_scripts/preprocess_objaverse.py \
-    --input data_samples/sample_objaverse \
-    --output data_samples/objaverse_processed \
-    --split test
-
+    --input /projects/vig/Datasets/objaverse/hf-objaverse-v1/rendered_dense \
+    --output /projects/vig/Datasets/objaverse/hf-objaverse-v1/lvsm_format \
+    --split train
 python preprocess_scripts/create_evaluation_index.py \
     --full-list data_samples/objaverse_processed/test/full_list.txt \
     --output data/evaluation_index_objaverse.json \
@@ -17,6 +21,19 @@ python preprocess_scripts/create_evaluation_index.py \
     --n-target 3 \
     --seed 42
 
+python preprocess_scripts/preprocess_objaverse.py \
+    --input data_samples/sample_objaverse \
+    --output data_samples/objaverse_processed \
+    --split test
+python preprocess_scripts/create_evaluation_index.py \
+    --full-list data_samples/objaverse_processed/test/full_list.txt \
+    --output data/evaluation_index_objaverse_test.json \
+    --n-input 2 \
+    --n-target 3 \
+    --seed 42
+
+# inference
+# base, re10k
 torchrun --nproc_per_node 1 --nnodes 1 \
 --rdzv_id 18635 --rdzv_backend c10d --rdzv_endpoint localhost:29506 \
 inference.py --config "configs/LVSM_scene_encoder_decoder.yaml" \
@@ -31,3 +48,19 @@ inference.if_inference = true \
 inference.compute_metrics = true \
 inference.render_video = true \
 inference_out_dir = ./experiments/evaluation/test
+
+# base, objaverse
+torchrun --nproc_per_node 1 --nnodes 1 \
+--rdzv_id 18635 --rdzv_backend c10d --rdzv_endpoint localhost:29506 \
+inference.py --config "configs/LVSM_scene_encoder_decoder.yaml" \
+training.dataset_path = "./data_samples/objaverse_processed/test/full_list.txt" \
+training.batch_size_per_gpu = 4 \
+training.target_has_input =  false \
+training.num_views = 5 \
+training.square_crop = true \
+training.num_input_views = 2 \
+training.num_target_views = 3 \
+inference.if_inference = true \
+inference.compute_metrics = true \
+inference.render_video = true \
+inference_out_dir = ./experiments/evaluation/test_objaverse
