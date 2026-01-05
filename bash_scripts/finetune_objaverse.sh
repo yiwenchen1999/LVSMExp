692e7e0efcb2b33d92d7c667fa63505ed1ceb7b7
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-01-04 21:09:22 -0500

    writing sbatch scripts

---
+++ bash_scripts/finetune_objaverse.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --partition=jiang
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:a6000:4
+#SBATCH --time=72:00:00
+#SBATCH --job-name=finetune_objaverse_lvsm
+#SBATCH --mem=32
+#SBATCH --ntasks=16
+#SBATCH --output=myjob.finetune_objaverse_lvsm.out
+#SBATCH --error=myjob.finetune_objaverse_lvsm.err
+export HF_HOME=/projects/vig/yiwenc/caches
+export HF_ACCELERATE_CONFIG_DIR=/projects/vig/yiwenc/caches/accelerate
+
+export WANDB_DIR=/scratch/chen.yiwe/wandb
+export WANDB_ARTIFACT_DIR=/scratch/chen.yiwe/wandb/artifacts
+export WANDB_CACHE_DIR=/scratch/chen.yiwe/wandb/cache
+export WANDB_CONFIG_DIR=/scratch/chen.yiwe/wandb/config
+
+export XDG_CACHE_HOME=/scratch/chen.yiwe/.cache
+export XDG_CONFIG_HOME=/scratch/chen.yiwe/.config
+export XDG_DATA_HOME=/scratch/chen.yiwe/.local/share
+
+cd /projects/vig/yiwenc/ResearchProjects/lightingDiffusion/3dgs/LVSMExp
+conda activate /projects/vig/yiwenc/all_env/rayzer
+
+torchrun --nproc_per_node 4 --nnodes 1 \
+    --rdzv_id 18635 --rdzv_backend c10d --rdzv_endpoint localhost:29502 \
+    train.py --config configs/LVSM_scene_encoder_decoder.yaml \
+    training.batch_size_per_gpu = 8
