12fe7dafdcee8bd435fcf62fd588a837cca22a3a
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-01-14 17:31:59 -0500

    launching exp for general dense traj relighting

---
+++ bash_scripts/relight_general_dense.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --partition=jiang
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:a6000:4
+#SBATCH --time=72:00:00
+#SBATCH --job-name=relight_general_dense
+#SBATCH --mem=32
+#SBATCH --ntasks=4
+#SBATCH --output=myjob.relight_general_dense.out
+#SBATCH --error=myjob.relight_general_dense.err
+export HF_HOME=/projects/vig/yiwenc/caches
+export HF_ACCELERATE_CONFIG_DIR=/projects/vig/yiwenc/caches/accelerate
+
+export WANDB_DIR=/scratch/chen.yiwe/wandb
+export WANDB_ARTIFACT_DIR=/scratch/chen.yiwe/wandb/artifacts
+export WANDB_CACHE_DIR=/scratch/chen.yiwe/wandb/cache
+export WANDB_CONFIG_DIR=/scratch/chen.yiwe/wandb/config
+
+export XDG_CACHE_HOME=/scratch/chen.yiwe/.cache
+export XDG_CONFIG_HOME=/scratch/chen.yiwe/.config
+export XDG_DATA_HOME=/scratch/chen.yiwe/.local/share
+
+torchrun --nproc_per_node 4 --nnodes 1 \
+    --rdzv_id 18635 --rdzv_backend c10d --rdzv_endpoint localhost:29502 \
+    train_editor.py --config configs/LVSM_scene_encoder_decoder_wEditor_general_dense.yaml \
+    training.batch_size_per_gpu = 4 
