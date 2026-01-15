61cc4ebd6662e1e13083f78189048163760abc92
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-01-15 10:43:18 -0500

    adding exp with a smaller lr

--- bash_scripts/relight_general_dense_lr1e4.sh
+++ bash_scripts/relight_general_dense_lr1e4.sh
@@ -28,4 +28,4 @@ torchrun --nproc_per_node 4 --nnodes 1 \
     training.LVSM_checkpoint_dir = ckpt/LVSM_scene_encoder_decoder \
     training.wandb_exp_name = LVSM_edit_dense_general \
     trainig.dataset_path = /projects/vig/Datasets/objaverse/hf-objaverse-v1/lvsm_with_envmaps/test/full_list.txt \
-    training.lr = 0.0002
+    training.lr = 0.0001
