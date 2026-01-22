8720e27007e0ec53770447b1d44749a3e3e99583
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-01-22 14:52:22 -0500

    adding finetune for res=512

--- bash_scripts/finetune_objaverse.sh
+++ bash_scripts/finetune_objaverse.sh
@@ -26,4 +26,5 @@ conda activate /projects/vig/yiwenc/all_env/rayzer
 torchrun --nproc_per_node 1 --nnodes 1 \
     --rdzv_id 18635 --rdzv_backend c10d --rdzv_endpoint localhost:29506 \
     train.py --config configs/LVSM_scene_encoder_decoder_512.yaml \
-    training.batch_size_per_gpu = 4
+    training.batch_size_per_gpu = 4 \
+    training.checkpoint_dir = ckpt/LVSM_scene_encoder_decoder
