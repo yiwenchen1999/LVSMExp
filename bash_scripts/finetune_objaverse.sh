16ad60c51223ce095ec6e211dce18ef987c46654
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-01-22 14:41:26 -0500

    adding finetune for res=512

--- bash_scripts/finetune_objaverse.sh
+++ bash_scripts/finetune_objaverse.sh
@@ -2,7 +2,7 @@
 #SBATCH --partition=jiang
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:a6000:4
-#SBATCH --time=72:00:00
+#SBATCH --time=96:00:00
 #SBATCH --job-name=finetune_objaverse_lvsm
 #SBATCH --mem=32
 #SBATCH --ntasks=16
@@ -23,7 +23,7 @@ export XDG_DATA_HOME=/scratch/chen.yiwe/.local/share
 cd /projects/vig/yiwenc/ResearchProjects/lightingDiffusion/3dgs/LVSMExp
 conda activate /projects/vig/yiwenc/all_env/rayzer
 
-torchrun --nproc_per_node 4 --nnodes 1 \
-    --rdzv_id 18635 --rdzv_backend c10d --rdzv_endpoint localhost:29502 \
-    train.py --config configs/LVSM_scene_encoder_decoder.yaml \
-    training.batch_size_per_gpu = 16
+torchrun --nproc_per_node 1 --nnodes 1 \
+    --rdzv_id 18635 --rdzv_backend c10d --rdzv_endpoint localhost:29506 \
+    train.py --config configs/LVSM_scene_encoder_decoder_512.yaml \
+    training.batch_size_per_gpu = 4
