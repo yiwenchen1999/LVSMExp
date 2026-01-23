86eaa56bf76fb0ec3f35688fe5544f2b7f5b9e29
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-01-22 22:22:20 -0500

    discard gradient acc

--- bash_scripts/finetune_objaverse.sh
+++ bash_scripts/finetune_objaverse.sh
@@ -27,4 +27,4 @@ torchrun --nproc_per_node 4 --nnodes 1 \
     --rdzv_id 18635 --rdzv_backend c10d --rdzv_endpoint localhost:29506 \
     train.py --config configs/LVSM_scene_encoder_decoder_512.yaml \
     training.batch_size_per_gpu = 4 \
-    training.grad_accum_steps = 2
+    training.grad_accum_steps = 1
