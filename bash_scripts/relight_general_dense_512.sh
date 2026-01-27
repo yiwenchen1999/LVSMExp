f9c689d56b6158767762b272f54be89d761ee3f5
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-01-27 00:00:40 -0500

    adding 512x512 relighting

--- bash_scripts/relight_general_dense_512.sh
+++ bash_scripts/relight_general_dense_512.sh
@@ -22,7 +22,7 @@ export XDG_DATA_HOME=/scratch/chen.yiwe/.local/share
 
 torchrun --nproc_per_node 4 --nnodes 1 \
     --rdzv_id 18635 --rdzv_backend c10d --rdzv_endpoint localhost:29501 \
-    train_editor.py --config configs/LVSM_scene_encoder_decoder_wEditor_general_dense.yaml \
+    train_editor.py --config configs/LVSM_scene_encoder_decoder_wEditor_general_dense_512.yaml \
     training.batch_size_per_gpu = 4 \
     training.checkpoint_dir = ckpt/LVSM_scene_encoder_decoder_wEditor_general_dense_lr1e4_singleMap \
     training.LVSM_checkpoint_dir = ckpt/LVSM_scene_encoder_decoder \
