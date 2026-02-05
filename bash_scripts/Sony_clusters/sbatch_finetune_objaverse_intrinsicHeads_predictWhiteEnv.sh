f033d8633209f49b565d0235af199c83ddee50e0
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-02-04 21:36:28 -0500

    training exp2

--- bash_scripts/Sony_clusters/sbatch_finetune_objaverse_intrinsicHeads_predictWhiteEnv.sh
+++ bash_scripts/Sony_clusters/sbatch_finetune_objaverse_intrinsicHeads_predictWhiteEnv.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=finetune_intrinsic_whiteenv
-#SBATCH --partition=ct
+#SBATCH --partition=sharedp
 #SBATCH --account=ct
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:2
 #SBATCH --time=168:00:00
 #SBATCH --output=/group2/ct/yiwen/logs/%x.%N.%j.out
 #SBATCH --error=/group2/ct/yiwen/logs/%x.%N.%j.err
@@ -68,12 +68,12 @@ srun singularity exec --nv $BIND $SIF bash -lc "
   export HF_ACCELERATE_CONFIG_DIR=\"$HF_ACCELERATE_CONFIG_DIR\"
   cd $PROJ
 
-  torchrun --nproc_per_node 4 --nnodes 1 \
+  torchrun --nproc_per_node 2 --nnodes 1 \
     --rdzv_id \$(date +%s) \
     --rdzv_backend c10d \
     --rdzv_endpoint localhost:29508 \
     train.py --config configs/LVSM_scene_encoder_decoder_wIntrinsicDecoder.yaml \
-    training.batch_size_per_gpu = 8 \
+    training.batch_size_per_gpu = 16 \
     training.white_env_as_albedo = true \
     training.checkpoint_dir = \"$CKPT_DIR\" \
     training.use_albedos = true \
