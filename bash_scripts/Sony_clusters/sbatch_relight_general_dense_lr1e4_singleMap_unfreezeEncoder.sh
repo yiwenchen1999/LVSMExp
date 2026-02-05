440fdc60dbbf9a855466e1288e42b2045aff4a65
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-02-04 21:40:52 -0500

    training exp2

--- bash_scripts/Sony_clusters/sbatch_relight_general_dense_lr1e4_singleMap_unfreezeEncoder.sh
+++ bash_scripts/Sony_clusters/sbatch_relight_general_dense_lr1e4_singleMap_unfreezeEncoder.sh
@@ -4,7 +4,7 @@
 #SBATCH --account=ct
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:2
-#SBATCH --time=24:00:00
+#SBATCH --time=72:00:00
 #SBATCH --output=/group2/ct/yiwen/logs/%x.%N.%j.out
 #SBATCH --error=/group2/ct/yiwen/logs/%x.%N.%j.err
 
