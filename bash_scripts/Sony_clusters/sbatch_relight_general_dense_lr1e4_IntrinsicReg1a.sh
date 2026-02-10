8213845fbf86937e33b1557eb485d23e9e72428b
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-02-09 20:24:06 -0500

    start training exp3b with bsz=8

--- bash_scripts/Sony_clusters/sbatch_relight_general_dense_lr1e4_IntrinsicReg1a.sh
+++ bash_scripts/Sony_clusters/sbatch_relight_general_dense_lr1e4_IntrinsicReg1a.sh
@@ -73,7 +73,7 @@ srun singularity exec --nv $BIND $SIF bash -lc "
     --rdzv_backend c10d \
     --rdzv_endpoint localhost:29501 \
     train_editor.py --config configs/LVSM_scene_encoder_decoder_wEditor_general_dense.yaml \
-    training.batch_size_per_gpu = 16 \
+    training.batch_size_per_gpu = 8 \
     training.checkpoint_dir = \"$CKPT_DIR\" \
     training.LVSM_checkpoint_dir = \"$LVSM_CKPT_DIR\" \
     training.wandb_exp_name = LVSM_edit_dense_general_lr1e4_IntrinsicReg3b \
