0e07e5f7c66a3140fdaab6c76e24b9dec71f9792
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-02-28 13:18:12 -0500

    test script on objaverse_novel

--- bash_scripts/inference_scripts/relight_general_dense_lr1e4.sh
+++ bash_scripts/inference_scripts/relight_general_dense_lr1e4.sh
@@ -24,7 +24,7 @@ torchrun --nproc_per_node 1 --nnodes 1 \
     --rdzv_id 18635 --rdzv_backend c10d --rdzv_endpoint localhost:29501 \
     train_editor.py --config configs/LVSM_scene_encoder_decoder_wEditor_general_dense.yaml \
     training.batch_size_per_gpu = 4 \
-    training.checkpoint_dir = ckpt/LVSM_scene_encoder_decoder_wEditor_general_dense_lr1e4 \
+    training.checkpoint_dir = ckpt/dense_relight_env \
     training.LVSM_checkpoint_dir = ckpt/LVSM_scene_encoder_decoder \
     training.dataset_path = /scratch/chen.yiwe/temp_objaverse/lvsm_with_envmaps/test/full_list.txt \
     training.wandb_exp_name = env_relight_objaverse \
