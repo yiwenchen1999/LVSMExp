520ac26f6ee53f4da9aa9c750a85fc7706d9b363
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-03-01 03:10:27 -0500

    launching finetune on relighting scenes

--- bash_scripts/VoltagePark/relight_general_dense_lr1e4_singleMap_scene.sh
+++ bash_scripts/VoltagePark/relight_general_dense_lr1e4_singleMap_scene.sh
@@ -42,7 +42,7 @@ echo "----------------------------------"
 
 torchrun --nproc_per_node "$NPROC_PER_NODE" --nnodes "$NNODES" \
     --rdzv_id "$(date +%s)" --rdzv_backend c10d --rdzv_endpoint localhost:29501 \
-    train_editor.py --config configs/LVSM_scene_encoder_decoder_wEditor_general_dense_512.yaml \
+    train_editor.py --config configs/LVSM_scene_encoder_decoder_wEditor_general_dense.yaml \
     training.batch_size_per_gpu = 16 \
     training.checkpoint_dir = "$CKPT_DIR" \
     training.dataset_path = "$DATA_LIST" \
