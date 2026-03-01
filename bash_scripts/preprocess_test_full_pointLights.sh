4205042c52c37d42944d0b95859f64d083c3e928
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-02-28 22:41:37 -0500

    add the post-processing of point lights for enhancements

--- bash_scripts/preprocess_test_full_pointLights.sh
+++ bash_scripts/preprocess_test_full_pointLights.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #SBATCH --partition=short
 #SBATCH --nodes=1
-#SBATCH --time=48:00:00
-#SBATCH --job-name=preprocess_train_short_list
+#SBATCH --time=24:00:00
+#SBATCH --job-name=preprocess_test_short_list
 #SBATCH --mem=32
 #SBATCH --ntasks=16
-#SBATCH --output=myjob.preprocess_train.out
-#SBATCH --error=myjob.preprocess_train.err
+#SBATCH --output=myjob.preprocess_test.out
+#SBATCH --error=myjob.preprocess_test.err
 
 
 python preprocess_scripts/preprocess_objaverse.py \
   --input /projects/vig/Datasets/objaverse/hf-objaverse-v1/rendered_dense_lightPlus \
   --output /scratch/chen.yiwe/temp_objaverse/lvsmPlus_objaverse \
   --output-tar /projects/vig/Datasets/objaverse/hf-objaverse-v1/lvsmPlus_objaverse_tar \
-  --split train \
+  --split test \
   --hdri-dir /projects/vig/Datasets/objaverse/envmaps_256/hdirs 
