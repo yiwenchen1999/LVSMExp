dd616117e2bef5e69d27fc7f0d55d7cae51cfa1b
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-03-01 13:56:49 -0500

    update pointlight tests

--- bash_scripts/Sony_clusters/interactive_train_editor_pointlight.sh
+++ bash_scripts/Sony_clusters/interactive_train_editor_pointlight.sh
@@ -20,7 +20,7 @@ export XDG_DATA_HOME=/scratch2/$USER/.local/share
 export HF_HOME=/scratch2/$USER/.cache/huggingface
 export HF_ACCELERATE_CONFIG_DIR=/scratch2/$USER/.cache/accelerate
 
-export DATA_LIST="/music-shared-disk/group/ct/yiwen/data/objaverse/polyhaven_lvsm/test/full_list_point_light.txt"
+export DATA_LIST="/music-shared-disk/group/ct/yiwen/data/objaverse/polyhaven_lvsm/test/full_list.txt"
 export CKPT_DIR="/music-shared-disk/group/ct/yiwen/codes/LVSMExp/ckpt/LVSM_scene_encoder_decoder_wEditor_pointlight"
 export LVSM_CKPT_DIR="/music-shared-disk/group/ct/yiwen/codes/LVSMExp/ckpt/LVSM_object_encoder_decoder_dense"
 
