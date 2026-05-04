55eb4af379db75ddb6e5b9d1d9b7041a9d195631
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-05-03 22:16:17 -0400

    correct config

--- bash_scripts/robustness_exps/relight_general_dense_lr1e4_quickInfer_robustness.sh
+++ bash_scripts/robustness_exps/relight_general_dense_lr1e4_quickInfer_robustness.sh
@@ -14,7 +14,7 @@ export HF_ACCELERATE_CONFIG_DIR=/projects/vig/yiwenc/caches/accelerate
 
 torchrun --nproc_per_node 1 --nnodes 1 \
     --rdzv_id 18635 --rdzv_backend c10d --rdzv_endpoint localhost:29503 \
-    train.py --config configs/LVSM_scene_encoder_decoder_general_dense.yaml \
+    train.py --config configs/LVSM_scene_encoder_decoder.yaml \
     training.batch_size_per_gpu = 1 \
     training.dataset_path = /projects/vig/Datasets/objaverse/hf-objaverse-v1/lvsmPlus_objaverse_robustTest_merged/test/full_list.txt \
     training.checkpoint_dir = ckpt/camera_robustness_test \
