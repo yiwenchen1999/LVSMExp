113e2c17c5fbf1a0cedceeeb35fb5fe9f41f5071
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-05-03 22:14:15 -0400

    script to run far-near pair

--- bash_scripts/robustness_exps/relight_general_dense_lr1e4_quickInfer_robustness.sh
+++ bash_scripts/robustness_exps/relight_general_dense_lr1e4_quickInfer_robustness.sh
@@ -14,15 +14,11 @@ export HF_ACCELERATE_CONFIG_DIR=/projects/vig/yiwenc/caches/accelerate
 
 torchrun --nproc_per_node 1 --nnodes 1 \
     --rdzv_id 18635 --rdzv_backend c10d --rdzv_endpoint localhost:29503 \
-    train_editor.py --config configs/LVSM_scene_encoder_decoder_wEditor_general_dense.yaml \
+    train.py --config configs/LVSM_scene_encoder_decoder_general_dense.yaml \
     training.batch_size_per_gpu = 1 \
     training.dataset_path = /projects/vig/Datasets/objaverse/hf-objaverse-v1/lvsmPlus_objaverse_robustTest_merged/test/full_list.txt \
-    training.checkpoint_dir = ckpt/progressive_results_formatchange \
-    training.LVSM_checkpoint_dir = ckpt/LVSM_scene_encoder_decoder \
-    training.wandb_exp_name = LVSM_edit_dense_general_lr1e4_robustness_quickInfer \
-    training.relight_signals = "[envmap]" \
-    training.multi_edit.enable = false \
-    training.multi_edit.max_steps = 1 \
+    training.checkpoint_dir = ckpt/camera_robustness_test \
+    training.wandb_exp_name = camera_robustness_test \
     training.warmup = 3000 \
     training.vis_every = 1 \
     training.lr = 0.0000
