3c7ffc1a6ca0f09d8c983badc257ff4ada7f8025
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-05-05 14:00:16 -0400

    obj-with-lighta

--- bash_scripts/realworld_exps/preprocess_obj_with_light_objaverse_like.sh
+++ bash_scripts/realworld_exps/preprocess_obj_with_light_objaverse_like.sh
@@ -7,8 +7,8 @@ source venv/bin/activate
 
 cd /Users/yiwenchen/Desktop/ResearchProjects/LightingDiffusion/3dgs/LVSMExp
 
-INPUT_ROOT="/Users/yiwenchen/Desktop/ResearchProjects/LightingDiffusion/3dgs/LVSMExp/data_samples/obj_with_light"
-OUTPUT_ROOT="/Users/yiwenchen/Desktop/ResearchProjects/LightingDiffusion/3dgs/LVSMExp/data_samples/processed_obj_with_light_objaverse_like"
+INPUT_ROOT="/projects/vig/Datasets/obj-with-light/dataset"
+OUTPUT_ROOT="/projects/vig/Datasets/obj-with-light/lvsm_format"
 
 python preprocess_scripts/preprocess_obj_with_light_objaverse_like.py \
   --input-root "${INPUT_ROOT}" \
