6c67b81f13688e4464b65aca6d0ef78c2063cacd
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-05-05 01:00:26 -0400

    sbatch with bsz=8

--- bash_scripts/img_quality_refinement/sony_sbatch_train_dense_512_lr1e4_singleMap.sh
+++ bash_scripts/img_quality_refinement/sony_sbatch_train_dense_512_lr1e4_singleMap.sh
@@ -88,7 +88,7 @@ srun singularity exec --nv $BIND $SIF bash -lc "
     --rdzv_backend c10d \
     --rdzv_endpoint localhost:29501 \
     train_editor.py --config configs/LVSM_scene_encoder_decoder_wEditor_general_dense_512_singleMap.yaml \
-    training.batch_size_per_gpu = 4 \
+    training.batch_size_per_gpu = 8 \
     training.dataset_path = \"$DATASET_PATH\" \
     training.checkpoint_dir = \"$CKPT_DIR\" \
     training.LVSM_checkpoint_dir = \"$LVSM_CKPT_DIR\" \
