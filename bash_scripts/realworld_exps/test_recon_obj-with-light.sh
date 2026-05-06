9a7a85f1c3b5a1ace68ed0d35e57c3b88f13dd4b
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-05-05 23:47:10 -0400

    relaunch training

--- bash_scripts/realworld_exps/test_recon_obj-with-light.sh
+++ bash_scripts/realworld_exps/test_recon_obj-with-light.sh
@@ -21,5 +21,5 @@ torchrun --nproc_per_node 1 --nnodes 1 \
     training.checkpoint_dir = ckpt/realworld_exps_obj-with-light \
     training.wandb_exp_name = realworld_exps_obj-with-light \
     training.warmup = 3000 \
-    training.vis_every = 1 \
+    training.vis_every = 1000 \
     training.lr = 0.0001
