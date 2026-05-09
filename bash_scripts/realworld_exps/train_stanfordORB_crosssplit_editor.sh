3af2b76ba73b2b0efbb9633677c48d7f03948f93
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-05-08 23:07:42 -0700

    test run stanford ORB exp

--- bash_scripts/realworld_exps/train_stanfordORB_crosssplit_editor.sh
+++ bash_scripts/realworld_exps/train_stanfordORB_crosssplit_editor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=jiang
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:a6000:1
+#SBATCH --gres=gpu:a6000:4
 #SBATCH --time=72:00:00
 #SBATCH --job-name=stanfordORB_crosssplit_editor
 #SBATCH --mem=128
@@ -12,11 +12,11 @@
 export HF_HOME=/projects/vig/yiwenc/caches
 export HF_ACCELERATE_CONFIG_DIR=/projects/vig/yiwenc/caches/accelerate
 
-torchrun --nproc_per_node 1 --nnodes 1 \
+torchrun --nproc_per_node 4 --nnodes 1 \
     --rdzv_id 18635 --rdzv_backend c10d --rdzv_endpoint localhost:29521 \
     train_editor.py --config configs/LVSM_scene_encoder_decoder_wEditor_stanfordORB_crosssplit.yaml \
-    training.dataset_path = data_samples/processed_stanford_ORB_objaverse_like/test/full_list.txt \
-    training.context_dataset_path = data_samples/processed_stanford_ORB_objaverse_like/train/full_list.txt \
+    training.dataset_path = /projects/vig/Datasets/stanfordORB/lvsm_stanford_orb/test/full_list.txt \
+    training.context_dataset_path = /projects/vig/Datasets/stanfordORB/lvsm_stanford_orb/train/full_list.txt \
     training.LVSM_checkpoint_dir = ckpt/realworld_exps \
     training.checkpoint_dir = ckpt/realworld_exps_relight_stanfordORB \
     training.wandb_exp_name = LVSM_edit_stanfordORB_crosssplit \
