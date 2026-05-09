b6d22031ec1b800a2fbe750e0b084e8d21c7fc0e
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-05-08 19:48:04 -0700

    preprocessing for stanfordORB

--- bash_scripts/img_quality_refinement/nebius_train_dense_256_dpt.sh
+++ bash_scripts/img_quality_refinement/nebius_train_dense_256_dpt.sh
@@ -27,9 +27,9 @@ torchrun --nproc_per_node 8 --nnodes 1 \
     --rdzv_id 28635 --rdzv_backend c10d --rdzv_endpoint localhost:29501 \
     train_editor.py --config configs/LVSM_scene_encoder_decoder_wEditor_general_dense_512_singleMap_dpt_transfer.yaml \
     training.batch_size_per_gpu = 16 \
-    training.checkpoint_dir = /mnt/data-disk/ckpt/dpt_decoder_256 \
-    training.dataset_path = /mnt/data-disk/lvsmPlus_objaverse/test/full_list.txt \
-    training.LVSM_checkpoint_dir = /mnt/data-disk/ckpt/dpt_decoder_256 \
+    training.checkpoint_dir = ckpt/dpt_decoder_256 \
+    training.dataset_path = /mnt/data-disk2/lvsmPlus_objaverse/test/full_list.txt \
+    training.LVSM_checkpoint_dir = ckpt/dpt_decoder_256 \
     training.wandb_exp_name = LVSM_edit_dense_general_256_dptTransfer \
     training.warmup = 1000 \
     training.vis_every = 1000 \
--- bash_scripts/img_quality_refinement/setup_nebius.sh
+++ bash_scripts/img_quality_refinement/setup_nebius.sh
@@ -37,24 +37,24 @@ mkdir ckpt/dpt_decoder_256
 rsync -avh --partial --inplace --progress \
   -e "ssh -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o TCPKeepAlive=yes" \
   chen.yiwe@xfer.discovery.neu.edu:/projects/vig/yiwenc/ResearchProjects/lightingDiffusion/3dgs/LVSMExp/ckpt/dense_relight_env_progressive/ckpt_0000000000026000.pt  \
-  ckpt/finetune512/
+  ckpt/dpt_decoder_256/
 
 # filetransfer
 rsync -avhP --partial --append-verify --info=progress2 --mkpath \
   -e "ssh -T" \
-  chen.yiwe@xfer.discovery.neu.edu:/scratch/chen.yiwe/temp_objaverse/lvsmPlus_objaverse/test/metadata/ \
-  /mnt/data-disk/lvsmPlus_objaverse/test/metadata/
+  chen.yiwe@xfer.discovery.neu.edu:/scratch/chen.yiwe/temp_objaverse/lvsmPlus_objaverse/test/envmaps/ \
+  /mnt/data-disk2/lvsmPlus_objaverse/test/envmaps/
 
 
 # mount disks:
 # Format it (this will erase anything on it - fine for a new disk)
-sudo mkfs.ext4 /dev/vdc
+sudo mkfs.ext4 /dev/vdd
 
 # Create mount point
-sudo mkdir -p /mnt/data-disk
+sudo mkdir -p /mnt/data-disk2
 
 # Mount it
-sudo mount /dev/vdc /mnt/data-disk
+sudo mount /dev/vdd /mnt/data-disk2
 
 # Give yourself access
-sudo chmod a+w /mnt/data-disk
+sudo chmod a+w /mnt/data-disk2
