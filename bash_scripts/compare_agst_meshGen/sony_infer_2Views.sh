b89aa69754eb34d7cdd21a9e3d1158d13397b79c
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-05-10 01:22:34 -0700

    2 view inferences

--- bash_scripts/compare_agst_meshGen/sony_infer_2Views.sh
+++ bash_scripts/compare_agst_meshGen/sony_infer_2Views.sh
@@ -30,8 +30,9 @@ export XDG_DATA_HOME=/scratch2/$USER/.local/share
 export HF_HOME=/scratch2/$USER/.cache/huggingface
 export HF_ACCELERATE_CONFIG_DIR=/scratch2/$USER/.cache/accelerate
 
-export DATASET_PATH="${DATASET_PATH:-/music-shared-disk/group/ct/yiwen/data/objaverse/polyhaven_lvsm/test/full_list.txt}"
-export CKPT_DIR="${CKPT_DIR:-$PROJ/ckpt/test_relight_2view}"
+export DATASET_PATH="${DATASET_PATH:-/music-shared-disk/group/ct/yiwen/data/objaverse/lvsm_with_envmaps/test/full_list.txt}"
+export CKPT_DIR="${CKPT_DIR:-$PROJ/ckpt/test_relight_2view_objaverse}"
+export RESUME_CKPT="${RESUME_CKPT:-$PROJ/ckpt/test_relight_2view/ckpt_00000000.pt}"
 export LVSM_CKPT_DIR="${LVSM_CKPT_DIR:-$PROJ/ckpt/LVSM_object_encoder_decoder_256}"
 export WANDB_EXP_NAME="${WANDB_EXP_NAME:-LVSM_edit_dense_general_256_lr1e4_singleMap}"
 
@@ -85,6 +86,8 @@ singularity exec --nv $BIND $SIF bash -lc "
     training.relight_signals = \"[envmap]\" \
     training.num_input_views = 2 \
     training.num_target_views = 4 \
+    training.resume_ckpt = \"$RESUME_CKPT\" \
+    training.reset_training_state = true \
     training.num_views = 6 \
     training.max_frame_dist = 30 \
     training.warmup = 3000 \
