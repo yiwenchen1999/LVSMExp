a0d2d13da09d88a09b1c48580d1d53b46e3c9dcb
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-06-01 03:21:07 -0700

    try to infer on point lights

--- bash_scripts/video_making/sony_sbatch_train_load_all_polyhaven_lvsm_recon_only.sh
+++ bash_scripts/video_making/sony_sbatch_train_load_all_polyhaven_lvsm_recon_only.sh
@@ -34,7 +34,7 @@ export HF_ACCELERATE_CONFIG_DIR=/scratch2/$USER/.cache/accelerate
 # Training controls
 ############################
 export DATASET_PATH="${DATASET_PATH:-/music-shared-disk/group/ct/yiwen/data/objaverse/polyhaven_lvsm/test/full_list.txt}"
-export CHECKPOINT_DIR="${CHECKPOINT_DIR:-$PROJ/ckpt/video_making_train_load_all_recon_only}"
+export CHECKPOINT_DIR="${CHECKPOINT_DIR:-$PROJ/ckpt_dpt/video_making_train_load_all_recon_only}"
 export RESUME_CKPT="${RESUME_CKPT:-$PROJ/ckpt_dpt/dpt_decoder_512_1e5}"
 export LVSM_CKPT_DIR="${LVSM_CKPT_DIR:-$PROJ/ckpt/LVSM_object_encoder_decoder_512}"
 export WANDB_EXP_NAME="${WANDB_EXP_NAME:-LVSM_video_making_train_load_all_recon_only}"
