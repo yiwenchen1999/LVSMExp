b700cf328dafbbb21d9df7f4a1868a21e5317f7e
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-06-02 06:14:08 -0700

    tune for 512x512

--- bash_scripts/realworld_exps/test_recon_stanford.sh
+++ bash_scripts/realworld_exps/test_recon_stanford.sh
@@ -16,7 +16,7 @@ export HF_ACCELERATE_CONFIG_DIR=/projects/vig/yiwenc/caches/accelerate
 torchrun --nproc_per_node 1 --nnodes 1 \
     --rdzv_id 18635 --rdzv_backend c10d --rdzv_endpoint localhost:29503 \
     train.py --config configs/LVSM_scene_encoder_decoder.yaml \
-    training.batch_size_per_gpu = 2 \
+    training.batch_size_per_gpu = 1 \
     training.dataset_path = "/projects/vig/Datasets/stanfordORB/lvsm_stanford_orb/train/full_list.txt,/projects/vig/Datasets/stanfordORB/lvsm_stanford_orb/test/full_list.txt" \
     training.dataset_name = data.dataset_scene_stanfordORB.Dataset \
     training.cross_split_relight = false \
