245952827e1b168b99f0fa6d7fc4d421c9814c2d
Yiwen Chen <yiwenchen@Yiwens-MacBook-Air.local>
2026-06-22 22:47:26 -0700

    generate samples for the paper only

--- bash_scripts/video_making/sony_sbatch_train_load_all_polyhaven_lvsm_recon_only.sh
+++ bash_scripts/video_making/sony_sbatch_train_load_all_polyhaven_lvsm_recon_only.sh
@@ -33,8 +33,8 @@ export HF_ACCELERATE_CONFIG_DIR=/scratch2/$USER/.cache/accelerate
 ############################
 # Training controls
 ############################
-export DATASET_PATH="${DATASET_PATH:-/music-shared-disk/group/ct/yiwen/data/objaverse/polyhaven_lvsm/test/full_list_env.txt}"
-export CHECKPOINT_DIR="${CHECKPOINT_DIR:-$PROJ/ckpt_dpt/video_making_polyhaven_env_all}"
+export DATASET_PATH="${DATASET_PATH:-/music-shared-disk/group/ct/yiwen/data/objaverse/polyhaven_lvsm/test/full_list_.txt}"
+export CHECKPOINT_DIR="${CHECKPOINT_DIR:-$PROJ/ckpt_dpt/video_making_polyhaven_paper_all}"
 export RESUME_CKPT="${RESUME_CKPT:-$PROJ/ckpt_dpt/video_making_train_load_all_recon_only}"
 export LVSM_CKPT_DIR="${LVSM_CKPT_DIR:-$PROJ/ckpt/LVSM_object_encoder_decoder_512}"
 export WANDB_EXP_NAME="${WANDB_EXP_NAME:-LVSM_video_making_train_load_all_recon_only}"
